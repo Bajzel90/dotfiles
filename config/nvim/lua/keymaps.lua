ffd03bfa3f9649a18b0921752188b0c3e61c39d0
Adam Hecht <adam.hecht@ing.com>
2024-06-19 16:00:54 +0200

    Remove unused mappings

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -8,13 +8,7 @@ map("n", "<C-u>", "<C-u>zz")
 map("n", "<C-d>", "<C-d>zz")
 map("n", "G", "Gzz")
 
-
-map(
-  "v",
-  "<leader>mt",
-  "!pandoc -t markdown-simple_tables<CR>",
-  { silent = true, desc = "Align selected md table" }
-)
+map("v", "<leader>mt", "!pandoc -t markdown-simple_tables<CR>", { silent = true, desc = "Align selected md table" })
 
 map("v", "<leader>mc", "<Cmd>MDTaskToggle<CR>", { desc = "Tasks" })
 map("n", "<leader>mm", "<Cmd>MDToc<CR>", { desc = "Markdown ToC" })
@@ -36,46 +30,33 @@ map("v", "K", ":m '<-2<CR>gv=gv")
 map("n", "<S-h>", "<Cmd>bp<CR>", { desc = "Buffer Previous" })
 map("n", "<S-l>", "<cmd>bn<CR>", { desc = "Buffer Next" })
 map("n", "<leader>bD", "<cmd>%bd|e#|bd#<cr>", { desc = "Close all but the current buffer" })
-map("n", "<leader>bd", "<cmd>Bdelete<cr>", { desc = "Close buffer" })
-map("n", "<leader><tab>", "<cmd>b#<cr>", { desc = "Previously openend Buffer" })
+map("n", "<leader>bd", "<cmd>bd<cr>", { desc = "Close buffer" })
 
 -- toggles
-map("n",
-  "<leader>tn",
-  function()
-    vim.o.relativenumber = vim.o.relativenumber == false and true or false
-  end,
-  { desc = "Toggle relative number" }
-)
+map("n", "<leader>tn", function()
+	vim.o.relativenumber = vim.o.relativenumber == false and true or false
+end, { desc = "Toggle relative number" })
 map("n", "<leader>th", function()
-  vim.print("Toggling hidden chars", vim.log.levels.INFO, "core.mappings")
-  vim.o.list = vim.o.list == false and true or false
+	vim.print("Toggling hidden chars", vim.log.levels.INFO, "core.mappings")
+	vim.o.list = vim.o.list == false and true or false
 end, { desc = "Toggle hidden chars" })
 map("n", "<leader>tl", function()
-  vim.print("Toggling signcolumn", vim.log.levels.INFO, "core.mappings")
-  vim.o.signcolumn = vim.o.signcolumn == "yes" and "no" or "yes"
+	vim.print("Toggling signcolumn", vim.log.levels.INFO, "core.mappings")
+	vim.o.signcolumn = vim.o.signcolumn == "yes" and "no" or "yes"
 end, { desc = "Toggle signcolumn" })
 map("n", "<leader>tv", function()
-  vim.print("Toggling virtualedit", vim.log.levels.INFO, "core.mappings")
-  vim.o.virtualedit = vim.o.virtualedit == "all" and "block" or "all"
+	vim.print("Toggling virtualedit", vim.log.levels.INFO, "core.mappings")
+	vim.o.virtualedit = vim.o.virtualedit == "all" and "block" or "all"
 end, { desc = "Toggle virtualedit" })
 map("n", "<leader>ts", function()
-  vim.print("Toggling spell", vim.log.levels.INFO, "core.mappings")
-  vim.o.spell = vim.o.spell == false and true or false
+	vim.print("Toggling spell", vim.log.levels.INFO, "core.mappings")
+	vim.o.spell = vim.o.spell == false and true or false
 end, { desc = "Toggle spell" })
 map("n", "<leader>tw", function()
-  vim.print("Toggling wrap", vim.log.levels.INFO, "core.mappings")
-  vim.o.wrap = vim.o.wrap == false and true or false
+	vim.print("Toggling wrap", vim.log.levels.INFO, "core.mappings")
+	vim.o.wrap = vim.o.wrap == false and true or false
 end, { desc = "Toggle wrap" })
 map("n", "<leader>tc", function()
-  vim.print("Toggling cursorline", vim.log.levels.INFO, "core.mappings")
-  vim.o.cursorline = vim.o.cursorline == false and true or false
+	vim.print("Toggling cursorline", vim.log.levels.INFO, "core.mappings")
+	vim.o.cursorline = vim.o.cursorline == false and true or false
 end, { desc = "Toggle cursorline" })
-map("n", "<leader>tO", "<cmd>lua require('utils.functions').toggle_colorcolumn()<cr>", { desc = "Toggle colorcolumn" })
-map(
-  "n",
-  "<leader>tt",
-  "<cmd>lua require('core.plugins.lsp.utils').toggle_virtual_text()<cr>",
-  { desc = "Toggle Virtualtext" }
-)
-map("n", "<leader>tS", "<cmd>windo set scb!<cr>", { desc = "Toggle Scrollbind" })
