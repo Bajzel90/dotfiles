a5437f82c60f43d224dfc9e8626b84a7c746e9e6
Adam Hecht <adamo156@gmail.com>
2024-08-23 14:15:31 +0200

    Update installation script

--- arch/install.sh
+++ arch/install.sh
@@ -11,12 +11,28 @@ install_yay_manager() {
     cd ~ || return
 }
 
+create_backup() {
+    _file="$1"
+    if [ -f "$_file" ]; then
+        mv "$_file" "$_file".bak
+    fi
+}
+
 download_dotfiles() {
+    cd ~ || exit
     git clone https://github.com/Bajzel90/dotfiles.git
+
+    create_backup ~/.zshrc
+    create_backup ~/.tmux.conf
+
+    ln -s ~/dotfiles/arch/zshrc ~/.zshrc
+    ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 }
 
 install_bluetooth() {
     sudo pacman -S bluez bluez-utils
+    systemctl enable bluetooth.service
+    systemctl start bluetooth.service
 }
 
 set_capslock_to_esc() {
@@ -32,35 +48,36 @@ install_zsh() {
     sudo pacman -S zoxide
 
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    ln -s ~/dotfiles/arch/zshrc ~/.zshrc
 }
 
 install_tmux() {
     pacman -S tmux
-    ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    tmux
+    ~/.tmux/plugins/tpm/bin/install_plugins
 }
 
 install_neovim() {
     sudo pacman -S neovim fd xclip nodejs npm ttf-hack-nerd fzf go python jq lazygit pandoc imagemagic luarocks ripgrep
     npm install --global poor-mans-t-sql-formatter-cli
 
-    FILE=~/.config/nvim
+    _file=~/.config/nvim
 
-    if [[ ! -f "$FILE" ]]; then
-        ln -s ~/dotfiles/config/nvim $FILE
-    else
-        rm $FILE
-        ln -s ~/dotfiles/config/nvim $FILE
-    fi
+    create_backup $_file
+    ln -s ~/dotfiles/config/nvim "$_file"
 }
 
 install_addtional_packages() {
-    sudo pacman -S keepassxc spotify-launcher remmina github-cli timeshift transmission-gtk cups
+    sudo pacman -s keepassxc spotify-launcher remmina github-cli timeshift transmission-gtk cups flameshot brightnessctl
 
-    yay -S forticlient-vpn
+    yay -s forticlient-vpn polybar feh rofi picom i3lock-color betterlockscreen
 }
+
+install_i3() {
+    sudo pacman -s flameshot brightnessctl
+
+    yay -s i3 polybar feh rofi picom i3lock-color betterlockscreen
+}
+
 set_capslock_to_esc
 download_dotfiles
 install_yay_manager
